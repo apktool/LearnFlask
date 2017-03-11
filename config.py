2a7abbbabc5eeb249b46c4485d0607c4344954f0
apktool <apktoolOo@gmail.com>
2017-03-11 20:09:49 +0800

    Add SQLAlchemy support

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 class basic_config(object):
     pass
 
+
 class prod_config(object):
     pass
 
+
 class dev_config(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:apktool@%:3306/myblog'
