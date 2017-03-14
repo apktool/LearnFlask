49f259308b5d5e90de2f84e2dc41af4d43e95828
apktool <apktoolOo@gmail.com>
2017-03-14 08:36:59 +0800

    Update wrong keywords spelled

--- models.py
+++ models.py
@@ -1,16 +1,17 @@
-from flask_script import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from main import app
 
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Colume(db.String(45), primary_key=True)
-    username = db.Colume(db.String(255))
-    password = db.Colume(db.String(255))
+    id = db.Column(db.String(45), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
 
-    def __init__(self, username):
+    def __init__(self, username, password):
         self.username = username
+        self.password = password
 
     def __repr__(self):
         return "<Model User '{}'>".format(self.username)
