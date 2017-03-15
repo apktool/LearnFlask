09a2d9bd0d3d34b675fd6259ca7ad957e2607cb6
apktool <apktoolOo@gmail.com>
2017-03-16 00:04:18 +0800

    Add post function

--- models.py
+++ models.py
@@ -9,6 +9,11 @@ class User(db.Model):
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
 
+    post = db.relationship(
+        'Post',
+        backref='users',
+        lazy='dynamic')
+
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
@@ -16,3 +21,21 @@ class User(db.Model):
 
     def __repr__(self):
         return "<Model User '{}'>".format(self.username)
+
+
+class Post(db.Model):
+    __tablename__='posts'
+
+    id = db.Column(db.String(45),primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime)
+
+    user_id = db.Column(db.String(45),db.ForeignKey('user.id'))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Model Post `{}`>".format(self.title)
+
