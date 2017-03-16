461d96f1f1fa55c8232819b7baa0e41793121d97
apktool <apktoolOo@gmail.com>
2017-03-16 23:30:50 +0800

    Add tags function

--- models.py
+++ models.py
@@ -23,6 +23,12 @@ class User(db.Model):
         return "<Model User '{}'>".format(self.username)
 
 
+posts_tags = db.Table('posts_tags',
+    db.Column('post_id', db.String(45), db.ForeignKey('posts.id')),
+    db.Column('tag_id', db.String(45), db.ForeignKey('tags.id'))
+)
+
+
 class Post(db.Model):
     __tablename__ = 'posts'
 
@@ -38,6 +44,11 @@ class Post(db.Model):
         backref='posts',
         lazy='dynamic')
 
+    tags = db.relationship(
+        'Tag',
+        secondary=posts_tags,
+        backref=db.backref('posts', lazy='dynamic'))
+
     def __init__(self, title):
         self.title = title
 
@@ -52,7 +63,21 @@ class Comment(db.Model):
     name = db.Column(db.String(255))
     text = db.Column(db.Text())
     date = db.Column(db.DateTime())
+
     post_id = db.Column(db.String(45), db.ForeignKey('posts.id'))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Model Comment `{}`>'.format(self.name)
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.String(45), primary_key=True)
+    name = db.Column(db.String(255))
 
     def __init__(self, name):
         self.name = name
