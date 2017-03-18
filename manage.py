0666942d26585f33ca2efd62e6ec9d80ae1affaf
apktool <apktoolOo@gmail.com>
2017-03-18 15:29:19 +0800

    Add Alembic in order to Database migration easily

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 import main
 import models
 
-manger = Manager(main.app)
-manger.add_command('server', Server())
+manager = Manager(main.app)
+manager.add_command('server', Server())
+migrate = Migrate(main.app, models.db)
+manager.add_command("db", MigrateCommand)
 
 
-@manger.shell
+@manager.shell
 def make_shell_context():
     return dict(app=main.app,
                 db=models.db,
@@ -17,7 +20,7 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
-    manger.run()
+    manager.run()
 
 
 '''
