a79f46491ce19fb330738defd8d386b47f6c24a7
apktool <apktoolOo@gmail.com>
2017-03-21 23:53:23 +0800

    Introduce view module

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from config import dev_config
 app = Flask(__name__)
 app.config.from_object(dev_config)
 
-
-@app.route('/')
-def home():
-    return '<h1>hello world</h1>'
-
+views = __import__('views')
 
 if __name__ == '__main__':
     app.run()
