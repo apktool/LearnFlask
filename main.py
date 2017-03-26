7a2aca9af1d893e76ba925e2f8c694e3a16c756c
apktool <apktoolOo@gmail.com>
2017-03-26 14:03:37 +0800

    Load basic_config module

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from config import dev_config
+from config import dev_config, basic_config
 import wt_forms
 
 app = Flask(__name__)
 app.config.from_object(dev_config)
+app.config.from_object(basic_config)
 
 views = __import__('views')
 
