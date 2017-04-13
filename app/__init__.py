c002ed60816212fbf7fcf43427806df72454efd5
apktool <apktoolOo@gmail.com>
2017-04-13 22:51:34 +0800

    Add confirm function

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 from config import config
 
 
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
+mail = Mail()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -20,6 +22,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
+    mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
