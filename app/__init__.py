054f0f712f627c3fc4b69c272ae6e6074020d233
apktool <apktoolOo@gmail.com>
2017-04-29 10:04:37 +0800

    Add MarkDown support

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_pagedown import PageDown
 from config import config
 
 
@@ -11,6 +12,7 @@ bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
 mail = Mail()
+PageDown = PageDown()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -26,6 +28,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    PageDown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
